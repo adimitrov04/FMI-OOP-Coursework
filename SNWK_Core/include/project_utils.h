dde77c9273c7b18d49dd71122666bc059b82e3d1
Antoan Dimitrov <antoan.dimitrov04@gmail.com>
2024-06-18 01:46:15 +0300

    Create header to include all utilities needed in the project at once

---
+++ SNWK_Core/include/project_utils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef __SNWK_PROJECT_UTILS_LIB__
+#define __SNWK_PROJECT_UTILS_LIB__
+
+#include "Utils/Vector.h"
+#include "Utils/String.h"
+#include "Utils/Sort.h"
+
+#include "Utils/Directory.h"
+
+#endif // __SNWK_PROJECT_UTILS_LIB__
