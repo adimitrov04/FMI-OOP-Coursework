ea5fbb8b98afc70ddb9684c7fd44b773771cd8e9
Antoan Dimitrov <antoan.dimitrov04@gmail.com>
2024-06-20 11:10:53 +0300

    Change AppElement abstraction to be ISerializable and added boilerplate serialization and deserialization methods

--- SNWK_Core/include/App/Interfaces/AppElement.h
+++ SNWK_Core/include/App/Interfaces/AppElement.h
@@ -3,7 +3,9 @@
 #ifndef __APP_ELEMENT_BASE__
 #define __APP_ELEMENT_BASE__
 
-class AppElement
+#include "ISerializeable.h"
+
+class AppElement : public ISerializeable
 {
 
 public:
@@ -13,10 +15,30 @@ public:
 public:
     virtual void DeleteObject () = 0;
     bool IsDeleted () const { return is_deleted_object; }
+    
+    virtual void Serialize (std::fstream& file) const override = 0;
+    virtual void Deserialize (std::fstream& file) override = 0;
+    
+    using ISerializeable::check_file_state;
 
 private:
     bool is_deleted_object;
 
 };
+
+void AppElement::Serialize (std::fstream& file) const
+{
+    uint8_t deletedByte = (is_deleted_object) ? 1 : 0;
+    file.write(reinterpret_cast<const char*>(&deletedByte), sizeof(deletedByte));
+    check_file_state(file);
+}
+
+void AppElement::Deserialize (std::fstream& file)
+{
+    uint8_t deletedByte = 0;
+    file.read(reinterpret_cast<char*>(&deletedByte), sizeof(deletedByte));
+    check_file_state(file);
+    is_deleted_object = deletedByte ? true : false;
+}
 
 #endif // __APP_ELEMENT_BASE__
