724c7aed9ff81ea9d14b0a832a8840722fd308b1
Antoan Dimitrov <antoan.dimitrov04@gmail.com>
2024-06-26 18:45:25 +0300

    Update Network class prototype

--- SNWK_Core/include/App/Network.h
+++ SNWK_Core/include/App/Network.h
@@ -3,8 +3,7 @@
 #ifndef __NETWORK_H__
 #define __NETWORK_H__
 
-#include "../Utils/Vector.h"
-#include "../Utils/String.h"
+#include "../project_utils.h"
 
 #include "User.h"
 #include "Comment.h"
@@ -21,25 +20,48 @@ public:
         return instance;
     }
 
-public:
-    User& GetCurrentUser () const;
-    void SetCurrentUser () const;
+    ~Network();
 
-    void Load (const String& dir_path);
+public:
+    User* GetCurrentUser () const noexcept;
+    User* GetUserByID (const uint32_t id) const noexcept;
+    User* GetUserByName (const String& name) const noexcept;
+    
+    Thread* GetThreadByID (const uint32_t id) const noexcept;
+    Thread* GetThreadByTitle (const String& title) const;
+    
+    const String& GetHomeDirectoryPath () const;
+
+    void OpenThread() const;
+    void OpenPost() const;
+
+    void SetCurrentUser (User& setUser) const;
+    void RegisterUser (const User& user);
+    void AddThread (const Thread& thread);
+
+    void LoadNetwork (String& dir_path);
+    void CreateNewNetwork (String& dir_path);
     void SaveInCurrentPath () const;
-    void ExportToNewPath (const String& export_path) const;
+    void ExportToNewPath (String& export_path) const;
+
+public:
+    static String home_directory_path;
 
 private:
     Network() = default;
     Network(const Network& other) = delete;
     Network& operator=(const Network& other) = delete;
-    ~Network() = default;
+
+    static bool fix_dir_path (String& path) noexcept;
 
 private:
-    static User& CURRENTLY_LOGGED_IN_USER;
-    
-    String directory_path;
+    static uint64_t currentUserCount;
 
+    static User* CURRENTLY_LOGGED_IN_USER;
+    static Thread* CURRENTLY_OPENED_THREAD;
+    static Post* CURRENTLY_OPENED_POST;
+
+private:
     Vector<User> users;
     Vector<Thread> threads;
 
